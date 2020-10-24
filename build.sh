1efaf0679f5d5a55d078ace66c57f833b12b6944
Arco Mul <me@arcomul.nl>
2020-10-24 19:40:53 +0200

    Update build script + move images to subfolder

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@
 # Merge header.html into index.html
 cat header.html >> index.html 
 
+shopt -s nullglob # Sets nullglob
+shopt -s nocaseglob # Sets nocaseglob
+
 # For each image in the folder print it into an <img> tag in index.html
-for filename in ./*.jpg; do
+for filename in ./cats/*.{jpg,jpeg,png}; do
     echo '    <img src="' $filename '"/>' >> index.html
 done
-
-# find . -name '*' -exec file {} \; | grep -o -P '^.+: \w+ image'
-
 
 # Merge footer.html into index.html
 cat footer.html >> index.html 
