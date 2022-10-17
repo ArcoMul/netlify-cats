288852cd3e55a3996516c701f9cb04102a2f4314
Huzthepro <hzyfakcay@gmail.com>
2022-10-17 15:31:39 +0200

    Cat names added to index html
    I dont know why I am doing this

--- build.sh
+++ build.sh
@@ -12,9 +12,25 @@ cat header.html >> index.html
 shopt -s nullglob # Sets nullglob
 shopt -s nocaseglob # Sets nocaseglob
 
+
+
 # For each image in the folder print it into an <img> tag in index.html
 for filename in ./cats/*.{jpg,jpeg,png}; do
-    echo '<div class="catContainer">    <img src="' $filename '"/> </div>' >> index.html
+
+shortName="${filename##*/}" # to clear the path in front of the name
+shortName="${shortName%.jpg}" # to clear .jpg after file name
+shortName="${shortName%.jpeg}" # to clear .jpeg after file name
+shortName="${shortName%.png}" # to clear .png after file name  
+#(this may not be the best solution but even this take solid 2 hours to manage. So.. I am okey with this)
+
+    echo '<div class="catContainer">' >> index.html # to position and size cat pictures easily
+    
+    echo '<h2 class="catName"> '$shortName' </h2>' >> index.html # for cat names
+
+    echo '<img src="' $filename '"/>' >> index.html # Original code
+    
+    echo '</div>' >> index.html # closing catContainer
+
 done
 
 # Merge footer.html into index.html
